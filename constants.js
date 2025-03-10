a17ae392c57e10c8a5c905c4a33bc7dfb9e253c0
Alexi Kurjenmäki <alexi.kurjenmaki@gmail.com>
2025-03-11 01:06:25 +0200

    Add files via upload

---
+++ constants.js
@@ -0,0 +1,11 @@
+export const API_URL = '/api/proxy?url=https://kitchen.kanttiinit.fi';
+
+export const DAYS = [
+    'Maanantai',
+    'Tiistai',
+    'Keskiviikko',
+    'Torstai',
+    'Perjantai',
+    'Lauantai',
+    'Sunnuntai',
+];
